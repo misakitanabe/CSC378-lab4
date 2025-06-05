0fcdf20318f291a06b958c8232217f0b08ad8669
misakitanabe <misaki.a.tanabe@gmail.com>
2025-06-04 21:20:49 -0700

    fixed healthbar bug

--- Assets/Scripts/Health/HealthBar.cs
+++ Assets/Scripts/Health/HealthBar.cs
@@ -9,8 +9,9 @@ public class HealthBar : MonoBehaviour
 
     private void Start()
     {
-        // the fillAmount dictates how many of the lives are colored in (not blacked out)
+        // the fillAmount dictates how many of the lives are showing
         currentHealthBar.fillAmount = playerHealth.currentHealth / 6;   // divided by 6 bc that's the max lives rn
+        totalHealthBar.fillAmount = playerHealth.currentHealth / 6;     // totalHealthBar is the black musicnotes that show under
     }
 
     private void Update()
