78e18c1af2da25748c6a09a10d1b8698995a5423
misakitanabe <misaki.a.tanabe@gmail.com>
2025-06-09 13:43:32 -0700

    made attack cool down larger

--- Assets/Scripts/GolemScript.cs
+++ Assets/Scripts/GolemScript.cs
@@ -63,7 +63,7 @@ public class GolemScript : MonoBehaviour
     {
         moveSpeed *= -1;
         transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
-        noteObject.transform.localScale = new Vector3(-noteObject.transform.localScale.x, -noteObject.transform.localScale.y, -noteObject.transform.localScale.z);
+        noteObject.transform.localScale = new Vector3(-noteObject.transform.localScale.x, noteObject.transform.localScale.y, noteObject.transform.localScale.z);
     }
 
     
